3ef4e5958f63c8924f86fc25968f16c8cc66246c
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2021-07-06 22:37:09 -0500

    seguimiento del proyecto

---
+++ views/index.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Oswald&display=swap" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo RUTA; ?>css/estilos.css">
+    <title>Blog Monse TIC</title>
+
+</head>
+<body>
+    <header>
+      <div class="contenedor">
+        <div class="logo izquierda">
+          <p><a href="#">Mi Primer Blog</a></p>
+        </div>
+
+        <div class="derecha">
+          <form name="busqueda" class="buscar" action="<?php echo RUTA;?>">
+            <input type="text" name="busqueda" placeholder="Buscar">
+            <button type="submit" class="icono fa fa-search"></button>
+          </form>
+
+          <nav class="menu">
+             <ul>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#">Contacto<i class="icono fa fa-envelope"></i></a></li>
+             </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+</body>
+</html>
